6106b0d9af932b44b95b3048938a33fff92a87f1
brianmodel <brian.model@gmail.com>
2018-11-09 16:48:55 -0500

    Updates

---
+++ precache-manifest.d7f9dd572bf99ce96948268dab8aeaf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dabbe3f6b5ff1fde51a7",
+    "url": "/static/js/main.dabbe3f6.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "dabbe3f6b5ff1fde51a7",
+    "url": "/static/css/main.83e0dc2a.chunk.css"
+  },
+  {
+    "revision": "bb27f1cdcfbc17b92eac3cda296ed42f",
+    "url": "/index.html"
+  }
+];
