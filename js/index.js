59063c75081c6b3886a73224c4bf3f502f1d7985
brianmodel <brian.model@gmail.com>
2018-12-17 23:11:35 -0500

    added buttons

--- js/index.js
+++ js/index.js
@@ -5,4 +5,19 @@ window.onload = function() {
       document.getElementById('name').textContent = nameStr.substr(0, document.getElementById('name').textContent.length + 1);
     }, 200 + i * 90);
   }
-}
+};
+
+$(document).ready(function() {
+
+  $("#homebutton").click(function() {
+    $('html,body').animate({
+      scrollTop: 0
+    }, 500);
+  });
+
+  $("#aboutbutton").click(function() {
+    $('html,body').animate({
+      scrollTop: $("#about").offset().top
+    }, 500);
+  });
+})
