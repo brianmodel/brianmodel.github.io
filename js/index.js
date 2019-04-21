59c6010e3bc47a631f8306d9b37c8431fafc8633
brianmodel <brian.model@gmail.com>
2019-04-21 15:03:08 -0400

    M I N I M A L

--- js/index.js
+++ js/index.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
     }, 500);
   });
 
-  $("#resumebutton").click(function() {
-    window.open("../resume.pdf");
+  $("#resume").click(function() {
+    window.open("./resume.pdf");
   });
 })
